98c659cbf49ba2faea4a386c45688b710143f5fa
bluecoder2003 <neelakshidas2003@gmail.com>
2023-11-20 11:32:24 +0530

    ft.home page

--- src/component/landing/Landing.jsx
+++ src/component/landing/Landing.jsx
@@ -24,23 +24,24 @@ const Landing = () => {
         </div>
       </div>
 
-      <div className="landing_content">
+      <div className="landing_content" id="home">
         <div className="text">
         <div className="content_text"> 
-        Bank securely, 
-anywhere, anytime.
+          Bank securely,anywhere, anytime.
         </div>
         <div className="content_text1"> 
-        Empowering your financial journey with seamless online 
-banking experiences tailored to your needs
+          Empowering your financial journey with seamless online banking experiences tailored to your needs
         </div> 
+        <button className="get_started_button">
+          Get Started
+        </button>
         </div>
         <div className="content_image">
             <img src={Image1} alt="Your Image" />
         </div>
       </div>
 
-      <div className="aboutus">
+      <div className="aboutus" id="about">
         <h2>About Us</h2>
       </div>
 
